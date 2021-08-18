7592904b198478ea7577ddd9670ee61e881daaed
manoviraj608 <81630616+manoviraj608@users.noreply.github.com>
2021-08-18 12:11:26 +0200

    Update firebaseInfo.js

--- settings/firebaseInfo.js
+++ settings/firebaseInfo.js
@@ -1,13 +1,13 @@
 // Initialize Firebase
 var config = {
-apiKey: "AIzaSyAnIhi-5qUxl9an1XMgfDiNY2eIdaD0qq4",
-  authDomain: "q-study.firebaseapp.com",
-  databaseURL: "https://q-study-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "q-study",
-  storageBucket: "q-study.appspot.com",
-  messagingSenderId: "462639363378",
-  appId: "1:462639363378:web:8910bc839850bb9e57532b",
-  measurementId: "G-3SZRCDG1C4"
+apiKey: "AIzaSyAOM3CAxNsBGm9HSVcbGg0km6C_kyNFwv0",
+    authDomain: "qonderzoek.firebaseapp.com",
+    databaseURL: "https://qonderzoek-default-rtdb.firebaseio.com",
+    projectId: "qonderzoek",
+    storageBucket: "qonderzoek.appspot.com",
+    messagingSenderId: "167026780855",
+    appId: "1:167026780855:web:a1754f3a04c34344f5fb32",
+    measurementId: "G-88Y9RXSMC4"
 };
 firebase.initializeApp(config);
 var rootRef = firebase.database().ref();
