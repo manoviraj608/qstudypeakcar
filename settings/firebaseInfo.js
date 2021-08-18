82eca20b8011c1a0351eda861be79c8c9d9819bd
manoviraj608 <81630616+manoviraj608@users.noreply.github.com>
2021-08-18 11:57:42 +0200

    try 2

--- settings/firebaseInfo.js
+++ settings/firebaseInfo.js
@@ -1,13 +1,14 @@
 // Initialize Firebase
 var config = {
- apiKey: "AIzaSyCuRIdG-GQoOhsIMylDAsUDkgBixoypYJA",
-    authDomain: "qstudy-cac5b.firebaseapp.com",
-    databaseURL: "https://qstudy-cac5b-default-rtdb.firebaseio.com",
-    projectId: "qstudy-cac5b",
-    storageBucket: "qstudy-cac5b.appspot.com",
-    messagingSenderId: "767354644619",
-    appId: "1:767354644619:web:9b451784c19146515dcc8e",
-    measurementId: "G-L8SCLBZGLJ"
+apiKey: "AIzaSyCuRIdG-GQoOhsIMylDAsUDkgBixoypYJA",
+authDomain: "qstudy-cac5b.firebaseapp.com",
+databaseURL: "https://qstudy-cac5b-default-rtdb.firebaseio.com",
+projectId: "qstudy-cac5b",
+storageBucket: "qstudy-cac5b.appspot.com",
+messagingSenderId: "767354644619",
+appId: "1:767354644619:web:9b451784c19146515dcc8e",
+measurementId: "G-L8SCLBZGLJ"
 };
 firebase.initializeApp(config);
 var rootRef = firebase.database().ref();
+
